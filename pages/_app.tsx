1c84435688cde20761768dea913e539331700e10
Martin Podloucký <podlouckymartin@gmail.com>
2022-12-26 10:59:02 +0100

    Fix app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import type { AppProps } from 'next/app'
 import '../styles.css';
 
-const App = ({ Component, pageProps }): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
