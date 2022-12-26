dbc6aff2ee2cfe77760c5d33e458dfaa12d4e7cd
Martin Podloucký <podlouckymartin@gmail.com>
2022-12-26 21:40:27 +0100

    Loading posts from database and filesystem

--- components/PostPreview/index.tsx
+++ components/PostPreview/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 interface Props {
+  slug: string,
   title: string,
   lead: string,
 };
 
-export const PostPreview = ({ title, lead }: Props): JSX.Element => {
+export const PostPreview = ({ slug, title, lead }: Props): JSX.Element => {
   return (
     <div className={styles.postPreview}>
-      <h2 className={styles.postPreviewTitle}>{title}</h2>
+      <Link href={`posts/${slug}`}>
+        <h2 className={styles.postPreviewTitle}>{title}</h2>
+      </Link>
       <p>{lead}</p>
     </div>
   );
---
+++ lib/mongodb.ts
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+}
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = client.connect();
+
+export interface Post {
+  slug: string;
+  title: string;
+  lead: string;
+}
+
+export const getAllPosts = async (): Promise<Post[]> => {
+  const connection = await clientPromise;
+  const db = connection.db('hyenydb');
+  return db.collection<Post>('posts').find().toArray();
+}
+
+export const getOnePost = async (slug: string): Promise<Post | null> => {
+  const connection = await clientPromise;
+  const db = connection.db('hyenydb');
+  const post = await db.collection<Post>('posts').findOne({ slug });
+
+  if (post === null) {
+    return null;
+  }
+  
+  return post;
+}
+
+
+export default clientPromise;
