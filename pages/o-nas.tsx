f821c70ee71a80b75140839b89c232e5d36ce813
Martin Podloucký <podlouckymartin@gmail.com>
2023-01-04 08:50:30 +0100

    Turn the about page to server rendered because of dark mode

--- pages/o-nas.tsx
+++ pages/o-nas.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetStaticPropsResult } from 'next';
+import { GetServerSidePropsResult } from 'next';
 import { fetchPublishers,  Publisher } from '../api-client';
 import PageLayout from '../components/PageLayout';
 import PublisherBio from '../components/PublisherBIo';
@@ -8,7 +8,7 @@ interface Props {
   publishers: Publisher[],
 }
 
-export const getStaticProps = async (): Promise<GetStaticPropsResult<Props>> => {
+export const getServerSideProps = async (): Promise<GetServerSidePropsResult<Props>> => {
   const publishers = await fetchPublishers();
   return {
     props: { publishers },
@@ -23,7 +23,7 @@ const HomePage = ({ publishers }: Props): JSX.Element => {
       <p>Během koronavirového lockdownu už nás přestalo bavit číst zprávy, protože v nich nebylo nic jiného než covid. Tak jsme se rozhodli udělat si vlastní. Formou mailového newsletteru posíláme (skoro) každý týden shrnutí a komentář zajímavostí, co se stalo ve světě a vy jste to možná nezaznamenali. Podmínkou je, že se nesmí jednat o nic spojeného s koronavirem.</p>
 
       <p>Nikdo z nás není novinář, nečekejte od nás tedy články odpovídající dobré žurnalistické praxi nebo nedejbože etice. Naše textíky by se spíš daly přirovnat k tomu, co vám povíme, když nám koupíte pivo v hospodě.</p>
-      
+
       {publishers.map((pub) => <PublisherBio key={pub.id} publisher={pub} />)}
     </PageLayout>
   );
