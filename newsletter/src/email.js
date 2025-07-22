cd1bbff1efd3b739a4648276ef57883dff92a59d
Martin Podloucký <podlouckymartin@gmail.com>
2025-07-22 20:48:04 +0200

    Starting completelly from scratch

--- newsletter/src/email.js
+++
@@ -1,5 +0,0 @@
-import { render } from './render';
-
-export const buildEmail = (emailData, recipientId) => render(
-  'mail.njk', { ...emailData, recipientId }
-);
--- newsletter/src/render.js
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import nunjucks from 'nunjucks';
-
-nunjucks.configure(path.resolve(__dirname, 'templates'));
-
-export const render = nunjucks.render;
